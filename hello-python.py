4ba3edec9e3cc5d26b0b26e24e3cb5ff62e624a8
AnthonyM214 <146883275+AnthonyM214@users.noreply.github.com>
2023-10-04 22:11:02 -0700

    Add files via upload

---
+++ hello-python.py
@@ -0,0 +1,8 @@
+    # The start of hello world
+
+
+def main():
+    print('Hi! My name is Anthony Adonai Martinez, and I go by he/him\n I would say the Matrix (1st only) and a chicken parmesan are my favorite movie and food.')
+
+if __name__=="__main__":
+    main()
