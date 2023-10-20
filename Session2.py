85bac45ce2b9314f0d9db10fc3a9fdb26dee5a4d
AnthonyM214 <146883275+AnthonyM214@users.noreply.github.com>
2023-10-20 00:54:20 -0700

    Session 2!

---
+++ Session2.py
@@ -0,0 +1,12 @@
+##Session 2
+import numpy as np
+
+x = 99.4 
+y = 2
+z = 7
+# + of 2 floats, - of 2 ints, * of float*int 
+
+print(f"The sum of {x} and {x} = {x + x}, and is a {type(x)}")
+print(f"The difference of {y} and {z} = {y-z}, and is a {type(z)}")
+a = (x*z)
+print(f"The product of {x} and {z} = {a}, and is a {type(a)}")
